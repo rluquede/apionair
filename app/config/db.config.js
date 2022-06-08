b2c2db167c9a626ba78f015059c38bedd0755f61
raullude <rluquede@gmail.com>
2022-06-08 18:30:18 +0200

    configuracion base de datos

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "onairbd"
+    HOST: "@us-cdbr-east-05.cleardb.net",
+    USER: "b82f06bd19becd",
+    PASSWORD: "28e28374",
+    DB: "heroku_90a8f75a8e747be"
 }
