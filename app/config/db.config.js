087fa2c17be7d68fb279f393a2698d7b00540767
raullude <rluquede@gmail.com>
2022-06-08 18:33:51 +0200

    cambio

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "@us-cdbr-east-05.cleardb.net",
+    HOST: "us-cdbr-east-05.cleardb.net",
     USER: "b82f06bd19becd",
     PASSWORD: "28e28374",
     DB: "heroku_90a8f75a8e747be"
